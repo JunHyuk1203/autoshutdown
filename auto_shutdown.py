fe80ace40be51014f1be748074dfb9ac53562ca9
JunHyuk1203 <JunHyuk1203@users.noreply.github.com>
2026-05-06 11:19:44 +0900

    🚀 Release v1.0.3

--- auto_shutdown.py
+++ auto_shutdown.py
@@ -16,7 +16,7 @@ import ctypes
 from ctypes import wintypes
 import subprocess
 
-CURRENT_VERSION = "1.0.2"
+CURRENT_VERSION = "1.0.3"
 
 try:
     from pycaw.pycaw import AudioUtilities
---
+++ login.cmd
@@ -0,0 +1,7 @@
+@echo off
+echo Please wait...
+echo A GitHub login window will open in your browser.
+echo Please click "Sign in with your browser" to authenticate.
+echo.
+"C:\Program Files\Git\cmd\git.exe" push -u origin main
+pause
