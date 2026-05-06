cd26677e9fb460ce900d80157284837830e8b92d
JunHyuk1203 <JunHyuk1203@users.noreply.github.com>
2026-05-06 13:12:20 +0900

    Release v1.0.13

--- auto_shutdown.py
+++ auto_shutdown.py
@@ -16,7 +16,7 @@ import ctypes
 from ctypes import wintypes
 import subprocess
 
-CURRENT_VERSION = "1.0.12"
+CURRENT_VERSION = "1.0.13"
 
 try:
     from pycaw.pycaw import AudioUtilities
@@ -208,16 +208,20 @@ class AutoShutdownAppV2:
                     try: os.remove(old_exe_path)
                     except: pass
                 
-                os.rename(current_exe, old_exe_path)
-                os.rename(update_exe_path, current_exe)
+                # PyInstaller 환경 변수 상속 및 핸들 잠금 방지를 위해 os.startfile 사용 (완전 독립 실행)
+                if hasattr(os, 'startfile'):
+                    try:
+                        os.startfile(current_exe, arguments="--wait-update")
+                    except TypeError:
+                        # Python 3.9 이하 호환성 대비
+                        env = os.environ.copy()
+                        for k in [k for k in env if k.upper() in ["TCL_LIBRARY", "TK_LIBRARY", "_MEIPASS2", "_MEIPASS"]]: env.pop(k, None)
+                        subprocess.Popen([current_exe, "--wait-update"], env=env, creationflags=subprocess.DETACHED_PROCESS)
+                else:
+                    env = os.environ.copy()
+                    for k in [k for k in env if k.upper() in ["TCL_LIBRARY", "TK_LIBRARY", "_MEIPASS2", "_MEIPASS"]]: env.pop(k, None)
+                    subprocess.Popen([current_exe, "--wait-update"], env=env, creationflags=subprocess.DETACHED_PROCESS)
                 
-                # PyInstaller 환경 변수 상속 방지 (새 프로세스가 삭제될 이전 프로세스의 임시 폴더를 참조하지 않도록 함)
-                env = os.environ.copy()
-                keys_to_remove = [k for k in env if k.upper() in ["TCL_LIBRARY", "TK_LIBRARY", "_MEIPASS2", "_MEIPASS"]]
-                for k in keys_to_remove:
-                    env.pop(k, None)
-                
-                subprocess.Popen([current_exe, "--wait-update"], env=env)
                 self.quit_app()
         except Exception as e:
             print("업데이트 적용 실패:", e)
