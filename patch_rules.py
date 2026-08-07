4703ad05437c68bb9713c750e624255a0f3495fd
JunHyuk1203 <JunHyuk1203@users.noreply.github.com>
2026-08-07 16:02:59 +0900

    Fix: Add update_info to database rules

---
+++ patch_rules.py
@@ -0,0 +1,13 @@
+﻿import json
+
+with open("database.rules.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+data["rules"]["update_info"] = {
+    ".read": "true",
+    ".write": "true"
+}
+
+with open("database.rules.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, indent=2)
+print("Updated database.rules.json")
