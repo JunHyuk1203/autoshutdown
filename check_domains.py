4963199a526f1193ee1c8f997d4b2a3ddd3527fd
JunHyuk1203 <JunHyuk1203@users.noreply.github.com>
2026-08-07 13:34:16 +0900

    Security: Add Content-Security-Policy (CSP) meta tag

---
+++ check_domains.py
@@ -0,0 +1,10 @@
+﻿import sys
+import re
+sys.stdout.reconfigure(encoding="utf-8")
+with open("index.html", "r", encoding="utf-8") as f:
+    text = f.read()
+
+domains = set(re.findall(r'https?://[a-zA-Z0-9.-]+', text))
+print("External domains used:")
+for d in domains:
+    print(d)
---
+++ check_tags.py
@@ -0,0 +1,9 @@
+﻿import sys
+import re
+sys.stdout.reconfigure(encoding="utf-8")
+with open("index.html", "r", encoding="utf-8") as f:
+    text = f.read()
+
+print("youtube match:", text.find("youtube"))
+print("fontawesome match:", text.find("fontawesome"))
+print("iframe match:", text.find("iframe"))
