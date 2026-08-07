ca41ed78516125dcf3417ba5cba49a26619beba0
JunHyuk1203 <JunHyuk1203@users.noreply.github.com>
2026-08-07 12:58:05 +0900

    Security: Implement global access state manager and add Firebase rules template

---
+++ check_authperm.py
@@ -0,0 +1,8 @@
+﻿import sys
+sys.stdout.reconfigure(encoding="utf-8")
+with open("index.html", "r", encoding="utf-8") as f:
+    text = f.read()
+
+start = text.find('async function _checkAuthPermission')
+end = text.find('}', start + 300)
+print(text[start:end+100])
---
+++ check_fetchPCData.py
@@ -0,0 +1,8 @@
+﻿import sys
+sys.stdout.reconfigure(encoding="utf-8")
+with open("index.html", "r", encoding="utf-8") as f:
+    text = f.read()
+
+start = text.find('function fetchPCData')
+end = text.find('function ', start + 100)
+print(text[start:end+200])
